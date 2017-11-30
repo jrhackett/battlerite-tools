6c552431dc848cf0b576bc2db4eccefba0180eb1
James Grippo <grippj@rpi.edu>
2017-11-29 20:37:52 -0500

    edited the seed data.

--- db/seed_data/champions.js
+++ db/seed_data/champions.js
@@ -1,27 +1,72 @@
 const champions = [
-  { name: 'Bakko' },
-  { name: 'Croak' },
-  { name: 'Freya' },
-  { name: 'Raigon' },
-  { name: 'Rook' },
-  { name: 'Ruh Kaan' },
-  { name: 'Shifu' },
-  { name: 'Thorn' },
-  { name: 'Ashka' },
-  { name: 'Destiny' },
-  { name: 'Ezmo' },
-  { name: 'Iva' },
-  { name: 'Jade' },
-  { name: 'Jumong' },
-  { name: 'Taya' },
-  { name: 'Varesh' },
-  { name: 'Blossom' },
-  { name: 'Lucie' },
-  { name: 'Oldur' },
-  { name: 'Pearl' },
-  { name: 'Pestilus' },
-  { name: 'Poloma' },
-  { name: 'Sirius' }
+  { name: 'Bakko',
+    roll: 'Melee' },
+
+  { name: 'Croak',
+    roll: 'Melee' },
+
+  { name: 'Freya',
+    roll: 'Melee' },
+
+  { name: 'Raigon',
+    roll: 'Melee' },
+
+  { name: 'Rook',
+    roll: 'Melee' },
+
+  { name: 'Ruh Kaan',
+    roll: 'Melee' },
+
+  { name: 'Shifu',
+    roll: 'Melee' },
+
+  { name: 'Thorn',
+    roll: 'Melee' },
+
+  { name: 'Ashka' ,
+    roll: 'Ranged'},
+
+  { name: 'Destiny',
+    roll: 'Ranged' },
+
+  { name: 'Ezmo',
+    roll: 'Ranged' },
+
+  { name: 'Iva',
+    roll: 'Ranged' },
+
+  { name: 'Jade',
+    roll: 'Ranged' },
+
+  { name: 'Jumong',
+    roll: 'Ranged' },
+
+  { name: 'Taya',
+    roll: 'Ranged' },
+
+  { name: 'Varesh',
+    roll: 'Ranged' },
+
+  { name: 'Blossom',
+    roll: 'Support' },
+
+  { name: 'Lucie',
+    roll: 'Support' },
+
+  { name: 'Oldur',
+    roll: 'Support' },
+
+  { name: 'Pearl',
+    roll: 'Support' },
+
+  { name: 'Pestilus',
+    roll: 'Support' },
+
+  { name: 'Poloma',
+    roll: 'Support' },
+
+  { name: 'Sirius',
+    roll: 'Support' }
 ]
 
 module.exports = champions
