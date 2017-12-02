ef13f73b09794b2b98db3a775e5bb5caf8bd0134
Jacob Hackett <jacobrhackett@gmail.com>
2017-12-01 21:17:52 -0500

    adding basic styles and a link in the navbar

--- src/components/navigation/TopNavigation.jsx
+++ src/components/navigation/TopNavigation.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import styles from './TopNavigation.scss'
 
 const TopNavigation = () => (
   <div className={ styles.topNav }>
-    <ul>
-      <li>Home</li>
+    <ul className={ styles.rightNav }>
+      <li><Link to='/'>Home</Link></li>
     </ul>
   </div>
 )
