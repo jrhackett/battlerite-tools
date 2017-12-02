6db67cec826d023ce3b5a5747edbd61a687fc7c2
Jacob Hackett <jacobrhackett@gmail.com>
2017-12-02 15:43:21 -0500

    adding uuid to battlerite seed data and adding builds in seed

--- db/seed.js
+++ db/seed.js
@@ -2,10 +2,20 @@ const db = require('../db')
 const champions = require('./seed_data/champions')
 const loadouts = require('./seed_data/loadouts')
 const battlerites = require('./seed_data/battlerites')
+const builds = require('./seed_data/builds')
+const uuidv4 = require('uuid/v4')
+
+const updateBattlerites = () => {
+  battlerites.map(battlerite => {
+    battlerite.uuid = uuidv4()
+  })
+}
 
 const seedChampions = () => db.Promise.map(champions, champion => db.model('champions').create(champion))
 
 const seedLoadouts = () => db.Promise.map(loadouts, loadout => db.model('loadouts').create(loadout))
+
+const seedBuilds = () => db.Promise.map(builds, build => db.model('builds').create(build))
 
 const seedBattlerites = () => db.Promise.map(battlerites, battlerite => db.model('battlerites').create(battlerite))
 
@@ -15,6 +25,9 @@ const seedBattlerites = () => db.Promise.map(battlerites, battlerite => db.model
    .then(champions => console.log(`Seeded ${ champions.length } champions OK`))
    .then(seedLoadouts)
    .then(loadouts => console.log(`Seeded ${ loadouts.length } loadouts OK`))
+   .then(seedBuilds)
+   .then(builds => console.log(`Seeded ${ builds.length } builds OK`))
+   .then(updateBattlerites)
    .then(seedBattlerites)
    .then(battlerites => console.log(`Seeded ${ battlerites.length } battlerites OK`))
    .catch(error => console.error(error))
