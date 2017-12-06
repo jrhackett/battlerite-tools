af5e0fc44b4b9e81dc34882e9fbdef44c0138916
Jacob Hackett <jacobrhackett@gmail.com>
2017-12-05 19:23:42 -0500

    adding some skeleton data for abilities

--- db/seed_data/abilities.js
+++ db/seed_data/abilities.js
@@ -1,3 +1,53 @@
-const abilities = []
+const bakko = [
+  { 
+    name: 'War Axe', 
+    description:'',
+    key: 'LMB'
+  },
+  {
+    name: 'Blood Axe',
+    description: '',
+    key: 'RMB'
+  },
+  {
+    name: 'Valiant Leap',
+    description: '',
+    key: 'SPACE'
+  },
+  {
+    name: 'War Stomp',
+    description: '',
+    key: 'SHIFT+SPACE'
+  },
+  {
+    name: 'Bulwark',
+    description: '',
+    key: 'Q'
+  },
+  {
+    name: 'Shield Dash',
+    description: '',
+    key: 'E'
+  },
+  {
+    name: 'Shield Slam',
+    description: '',
+    key: 'SHIFT+E'
+  },
+  {
+    name: 'War Shout',
+    description: '',
+    key: 'R'
+  },
+  {
+    name: 'Heroic Charge',
+    description: '',
+    key: 'F'
+  }
+]
+
+const abilities = [
+  ...bakko
+]
 
 module.exports = abilities
