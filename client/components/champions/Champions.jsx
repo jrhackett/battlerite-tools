2fd6f6342f9385ea5fdce89db155cacd2247566c
Jacob Hackett <jacobrhackett@gmail.com>
2017-12-19 17:56:36 -0500

    refactoring champions to stateless functional component

--- client/components/champions/Champions.jsx
+++ client/components/champions/Champions.jsx
@@ -1,29 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import AbilitiesContainer from '../../containers/abilities/AbilitiesContainer'
 import styles from './Champions.scss'
 
-class Champions extends Component {
-  render() {
-    return (
-      <div>
-        <div className={ styles.championsContainer }>
-          {this.props.champions.map(champion => {
-            return (
-              <div className={ champion.id === this.props.activeChampion ? styles.activeChampionContainer : styles.championContainer } key={ champion.id }>
-                <Link to={ `${ this.props.linkHref }/${ champion.id }` }>
-                  <img 
-                    src={ require(`../../assets/images/champions/${ champion.name.toLowerCase() }/icon.png`) } 
-                    alt={ `${ champion.name }-icon` } />
-                </Link>
-              </div>
-            )
-          })}
-        </div>
-        <AbilitiesContainer activeChampion={ this.props.activeChampion } activeChampionName={ this.props.activeChampionName } />
-      </div>
-    )
-  }
-}
+const Champions = ({ champions, activeChampion, activeChampionName, linkHref }) => (
+  <div>
+    <div className={ styles.championsContainer }>
+      {champions.map(champion => {
+        return (
+          <div className={ champion.id === activeChampion ? styles.activeChampionContainer : styles.championContainer } key={ champion.id }>
+            <Link to={ `${ linkHref }/${ champion.id }` }>
+              <img 
+                src={ require(`../../assets/images/champions/${ champion.name.toLowerCase() }/icon.png`) } 
+                alt={ `${ champion.name }-icon` } />
+            </Link>
+          </div>
+        )
+      })}
+    </div>
+    <AbilitiesContainer activeChampion={ activeChampion } activeChampionName={ activeChampionName } />
+  </div>
+)
 
 export default Champions
