4c5d26e26c872630cd894a3efc2e54550c9ccd8d
Jacob Hackett <jacobrhackett@gmail.com>
2017-12-24 17:20:54 -0500

    fixing typos and setting up for form local state

--- client/components/loadouts/LoadoutForm.jsx
+++ client/components/loadouts/LoadoutForm.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ChampionsContainer from '../../containers/champions/ChampionsContainer'
 import BattleritesContainer from '../../containers/battlerites/BattleritesContainer'
 
-const LoadoutForm = ({ actionChampion }) => (
-  <div>
-    <h2>Create a Loadout</h2>
-    <ChampionsContainer />
-    <BattleritesContainer activeChampion={ actionChampion } />
-  </div>
-)
+class LoadoutForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      battlerite: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Create a Loadout</h2>
+        <ChampionsContainer />
+        <BattleritesContainer activeChampion={ this.props.activeChampion } />
+      </div>
+    )
+  }
+}
 
 export default LoadoutForm
--- client/containers/loadouts/CreateLoadoutContainer.js
+++ client/containers/loadouts/CreateLoadoutContainer.js
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom'
 import LoadoutForm from '../../components/loadouts/LoadoutForm'
 
 const mapStateToProps = (state, ownProps) => ({
-  actionChampion: parseInt(ownProps.match.params.champion, 10)
+  activeChampion: parseInt(ownProps.match.params.champion, 10)
 })
 
 export default withRouter(connect(mapStateToProps)(LoadoutForm))
