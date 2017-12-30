6964e016eaa22971ddab3edd661ecae1a1f445cb
Jacob Hackett <jacobrhackett@gmail.com>
2017-12-30 17:13:11 -0500

    removing the .gg from the brand

--- client/components/navigation/TopNavigation.jsx
+++ client/components/navigation/TopNavigation.jsx
@@ -6,7 +6,7 @@ import styles from './TopNavigation.scss'
 const TopNavigation = () => (
   <div className={ styles.topNav }>
     <div className={ styles.brand }>
-      <Link to="/">Battleright.gg</Link>
+      <Link to="/">Battleright</Link>
     </div>
     <ul className={ styles.rightNav }>
       <li><Link to='/champions'>Champions</Link></li>
